87e60fb89c97f52f7c7205417b7ce935185da210
Jingfei Shi <js868a@us.att.com>
2016-03-28 17:56:51 -0400

    updated skeleton code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,35 +1,18 @@
 'use strict';
 
 /**
- * @ngdoc overview
- * @name whenigrowupApp
- * @description
- * # whenigrowupApp
- *
- * Main module of the application.
- */
-angular
-  .module('whenigrowupApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+* @ngdoc overview
+* @name whenigrowupApp
+* @description
+* # whenigrowupApp
+*
+* Main module of the application.
+*/
+angular.module('whenigrowupApp', [
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngTouch',
+  'ui.router'
+]);
