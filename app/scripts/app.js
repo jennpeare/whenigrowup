3defe3e96a46da2595cc256ad7af73ba5e7eb384
Jingfei Shi <js868a@us.att.com>
2016-04-04 15:01:21 -0400

    commit

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+* @ngdoc overview
+* @name whenigrowupApp
+* @description
+* # whenigrowupApp
+*
+* Main module of the application.
+*/
+angular.module('whenigrowupApp', [
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngTouch',
+  'ui.router'
+]);
