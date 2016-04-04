d201ebbe525f46ddbeaf7f8db32be00ca69883de
Jingfei Shi <js868a@us.att.com>
2016-04-04 12:05:30 -0400

    modified character picking page

--- app/scripts/controllers/splash.js
+++ app/scripts/controllers/splash.js
@@ -4,6 +4,7 @@ var app = angular.module('whenigrowupApp');
 
 app.controller('SplashCtrl', function($state, $timeout) {
   var splash = this;
+
   splash.fade = false;
 
   splash.transition = function() {
@@ -11,4 +12,33 @@ app.controller('SplashCtrl', function($state, $timeout) {
   };
 
   $timeout(splash.transition, 2000);
+
+  splash.selected = false;
+
+  splash.characters = [
+    {
+      name: 'Skye',
+      occupation: 'Developer',
+      avatar: '../../images/Developer.png',
+      selected: false
+    },
+    {
+      name: 'Leo',
+      occupation: 'Engineer',
+      avatar: '../../images/Engineer.png',
+      selected: false
+    },
+    {
+      name: 'Jemma',
+      occupation: 'Biologist',
+      avatar: '../../images/Biologist.png',
+      selected: false
+    }
+  ];
+
+  splash.pick = function() {
+    splash.characters[0].selected = true;
+    console.log(splash.characters[0]);
+  }
+
 });
